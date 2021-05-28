e1b5a6300916dd48c89abee0796d79bfe4a84961
Surbhit Vishwakarma <83189259+surbhit-7@users.noreply.github.com>
2021-05-29 02:41:19 +0530

    Create maximumSubarraySum.cpp

---
+++ maximumSubarraySum.cpp
@@ -0,0 +1,41 @@
+// Kadence ALgorithm 
+// Difficulty - easy
+//problem statement :- 
+/*
+53. Maximum Subarray
+Easy
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+
+*/
+// Code :-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sums = nums[0] , maxSum = nums[0];
+        for(int i = 1 ; i < nums.size() ; i++)
+        {
+            sums = max(sums + nums[i] , nums[i]);
+            maxSum = max(sums,maxSum);
+        }
+        return maxSum;
+    }
+};
